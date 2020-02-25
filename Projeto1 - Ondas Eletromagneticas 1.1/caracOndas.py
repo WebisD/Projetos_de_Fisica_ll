1141c4d183140d730bb2de0b8844e7877291f8ff
Webis <49571908+ronvard@users.noreply.github.com>
2020-02-24 23:15:38 -0300

    Add files via upload

---
+++ Projeto1 - Ondas Eletromagneticas 1.1/caracOndas.py
@@ -0,0 +1,79 @@
+from scipy.constants import c, pi
+import numpy as np
+
+def numeroDaOnda(opcao, entrada):
+    if opcao == 1:                  # entrada = frequencia
+        nOnda = (2*pi*entrada)/c
+    elif opcao == 2:                # entrada = comprimento de onda
+        nOnda = (2*pi)/entrada
+
+    print("--> Numero da Onda: "+ np.format_float_scientific(np.float32(nOnda), precision=3) + " rad/m")
+
+    return
+
+def freqAngular(opcao, entrada):
+    if opcao == 1:                # entrada = frequencia
+        w = 2*pi*entrada
+    elif opcao == 2:              # entrada = comprimento de onda
+        w = (2*pi*c)/entrada
+
+    print("--> Frequência Angular: " + np.format_float_scientific(np.float32(w), precision=3) + " rad/s")
+
+    return
+
+def analisaUnidade(unidade):
+    valor = 1
+
+    if unidade == "nm":
+        valor = 10**-9
+    elif unidade == "mm":
+        valor = 10**-6
+    elif unidade == "m":
+        valor = 1
+    elif unidade == "km":
+        valor = 10**3
+    else:
+        print("Ta errado :(")
+        tipoEscolhido(2)
+    return valor
+
+def tipoEscolhido(opcaoEntr):
+
+    if opcaoEntr == 1:
+        escolha = input("\nDigite a frequência (Ex. 700 Hz): ")
+    elif opcaoEntr == 2:
+        escolha = input("\nDigite o comprimento de onda (Ex. 700 km): ")
+
+    valor, unidade = escolha.split(" ")
+    valor = float(valor)
+
+    if opcaoEntr == 2:
+        expo = analisaUnidade(unidade)
+        valor *= expo
+
+    return valor
+
+def menu():
+    print("\nEntrar com:")
+    print("\t1 - Frequência")
+    print("\t2 - Comprimento de Onda")
+
+    return
+
+def pedeValor(opcaoCarac):
+
+    while True:
+        menu()
+        opcaoEntr = int(input("Digite uma opção: "))
+
+        if (opcaoEntr not in (1, 2)):
+            continue
+
+        tipo = tipoEscolhido(opcaoEntr)
+
+        if opcaoCarac == 1:
+            numeroDaOnda(opcaoEntr, tipo)
+            break
+        elif opcaoCarac == 2:
+            freqAngular(opcaoEntr, tipo)
+            break
---
+++ Projeto1 - Ondas Eletromagneticas 1.1/freqToComp.py
@@ -0,0 +1,61 @@
+from scipy.constants import c
+
+def classificarUnidade(tipo):
+
+    unidade = ""
+    valor = 1
+    #ver se nao é expoente negativo
+    if tipo == "Ondas de Rádio":
+        unidade = "m"
+        valor = 1
+    elif tipo == "Micro-Ondas":
+        unidade = "mm"
+        valor = 10 ** - 3
+    elif tipo in range("Infravermeho", "Raios X", "Raios Gama", "Ultravioleta", "Visível"):
+        unidade = "nm"
+        valor = 10 ** -9
+
+    return unidade, valor
+
+def classificarOnda(comp):
+    tipo = ""
+
+    if (comp >= 1.0 * 10 ** -(0.5)):
+        tipo = "Ondas de Rádio"
+    elif (comp < 1.0 * 10 ** -(0.5) and comp >= 1.0 * 10 ** -3):
+        tipo = "Micro-Ondas"
+    elif (comp < 1.0 * 10 ** -3 and comp >= 400 * 10 ** -9):
+        tipo = "Infravermeho"
+    elif (comp < 400.0 * 10 ** -9 and comp >= 700.0 * 10 ** -9):
+        tipo = "Visível"
+    elif (comp < 700 * 10 ** -9 and comp >= 400.0 * 10 ** -7.5):
+        tipo = "Ultravioleta"
+    elif (comp < 1.0 * 10 ** -7.5 and comp >= 1.0 * 10 ** -10.5):
+        tipo = "Raios X"
+    elif (comp < 1.0 * 10 ** -10.5 and comp >= 1.0 * 10 ** -12):
+        tipo = "Raios Gama"
+
+    unidade, valor = classificarUnidade(tipo)
+
+    return tipo, unidade, valor
+
+def CalculaFrequencia(freq):
+
+    comp = c/freq
+
+    tipo, unidade, valor = classificarOnda(comp)
+
+    comp *= valor
+
+    print("--> Comprimento de Onda: " + str(comp) + " " + unidade)
+    print("--> Tipo de Onda: " + tipo + "\n")
+
+    return
+
+def pedeFrequencia():
+
+    freq = input("\nDigite a frequência (Ex. 700 Hz): ")
+
+    valor = float(freq[0:-3])
+
+    CalculaFrequencia(valor)
