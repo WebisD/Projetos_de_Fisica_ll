844dee98522b26ab9e8755f4c62d8d655f3033b4
Webis <49571908+ronvard@users.noreply.github.com>
2020-03-06 19:10:05 -0300

    Add files via upload

--- Projeto1 - Ondas Eletromagneticas 1.1/clickme.exe.bat
+++ Projeto1 - Ondas Eletromagneticas 1.1/clickme.exe.bat
@@ -1,5 +1,10 @@
 ECHO OFF
+python -m pip install --upgrade pip wheel setuptools
+python -m pip install docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew
+python -m pip install kivy.deps.gstreamer
+python -m pip install kivy.deps.angle
+python -m pip install kivy
 python -m pip install scipy
 python -m pip install numpy
-python main.py
+python main2.py
 PAUSE
