9341f19e1d1c8b4f2afc952a0270738280e6e91a
Webis <49571908+ronvard@users.noreply.github.com>
2020-03-11 21:06:23 -0300

    Add files via upload

--- Projeto1 - Ondas Eletromagneticas 1.1/compToFreq.py
+++ Projeto1 - Ondas Eletromagneticas 1.1/compToFreq.py
@@ -1,6 +1,25 @@
 from scipy.constants import c
 import numpy as np
-#c = 3*10**8
+
+def classificarOnda(comp):
+    tipo = ""
+
+    if (comp >= 1.0 * 10 ** -(0.5)):
+        tipo = "Ondas de Rádio"
+    elif (comp < 1.0 * 10 ** -(0.5) and comp >= 1.0 * 10 ** -3):
+        tipo = "Micro-Ondas"
+    elif (comp < 1.0 * 10 ** -3 and comp >= 400 * 10 ** -9):
+        tipo = "Infravermeho"
+    elif (comp < 400.0 * 10 ** -9 and comp >= 700.0 * 10 ** -9):
+        tipo = "Visível"
+    elif (comp < 700 * 10 ** -9 and comp >= 400.0 * 10 ** -7.5):
+        tipo = "Ultravioleta"
+    elif (comp < 1.0 * 10 ** -7.5 and comp >= 1.0 * 10 ** -10.5):
+        tipo = "Raios X"
+    elif (comp < 1.0 * 10 ** -10.5 and comp >= 1.0 * 10 ** -12):
+        tipo = "Raios Gama"
+
+    return tipo
 
 def analisaUnidade(unidade):
     valor = 1
@@ -19,9 +38,11 @@ def analisaUnidade(unidade):
 
 def calculaFrequencia(comp):
     f = c/comp
+    tipo = classificarOnda(comp)
 
     #print("Frequência: " + "{:e}".format(f) + " Hz")
     print("--> Frequência: " + np.format_float_scientific(np.float32(f), precision=3) + " Hz")
+    print("--> Tipo de Onda: " + tipo + "\n")
 
     return
 
